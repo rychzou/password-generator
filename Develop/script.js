ccda9c4a5dd6ac5e054f919a9f9b8f8fee88d978
rychzou <richardzou25@gmail.com>
2023-03-21 04:32:22 -0500

    JS file initial declarations and functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,40 @@
 // Assignment code here
+const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChar = "0123456789";
+const specialChar = "!@#$%&*()^)_+=-,./<>?{}[]"
 
+// Function for prompts
+function getPSasswordCriteria() {
+  var length = prompt("Choose a password length (between 8 and 128 characters).")
+  while (length < 8 || length > 128 || isNaN(length)) {
+    length = prompt("Please choose a valid password length (between 8 and 128 characters).");
+  }
 
+  var includeLowerCase = confirm("Do you want to include lowercase characters?");
+  var includeUpperCase = confirm("Do you want to include uppercase characters?");
+  var includeNumeric = confirm("Do you want to include numbers?");
+  var includeSpecialChars = confirm("Do you want to include special characters?");
+
+  while (!(includeLowerCase || includeUpperCase || includeNumeric || includeSpecialChars)) {
+    alert("Please select at least one character type to include in your password.");
+    includeLowerCase;
+    includeUpperCase;
+    includeNumeric;
+    includeSpecialChars;
+  }
+
+  var passwordCriteria = {
+    length: length,
+    includeLowerCase: includeLowerCase,
+    includeUpperCase: includeUpperCase,
+    includeNumeric: includeNumeric,
+    includeSpecialChars: includeSpecialChars
+  };
+
+  return passwordCriteria;
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
